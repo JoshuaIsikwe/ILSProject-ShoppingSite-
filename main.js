11f364444d63345d2de9cb98efcb56614e3e8558
Sibongisipho Dlova <sibongisiphodlova@gmail.com>
2020-05-15 05:49:45 +0200

    CleanUpJob@90%

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*Sign Up Page */
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
